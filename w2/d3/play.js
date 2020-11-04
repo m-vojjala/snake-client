e9b65488ec5d567a118a32d1ccf2b82e5b2feeee
MounikaVojjala <mounikavojjala@gmail.com>
2020-11-04 20:40:56 +0000

    stdin setup

--- w2/d3/play.js
+++ w2/d3/play.js
@@ -1,8 +1,35 @@
 const net = require('net');
+const stdin = process.stdin;
+
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
 
 /**
  * Establishes connection with the game server
  */
 const { connect } = require('./client');
+
+const handleUserInput = function(){
+  stdin.on('data',(key)=>{
+if(key === '\u0003'){
+  process.exit();
+}
+  })
+}
+
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+setupInput()
+handleUserInput()
+
 console.log('Connecting ...');
 connect();
