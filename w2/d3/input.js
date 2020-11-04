7807fded706662061f64a99a55bf3501c9842a91
MounikaVojjala <mounikavojjala@gmail.com>
2020-11-04 22:44:29 +0000

    added input file

---
+++ w2/d3/input.js
@@ -0,0 +1,26 @@
+
+
+
+
+const handleUserInput = function(){
+  stdin.on('data',(key)=>{
+if(key === '\u0003'){
+  process.exit();
+}
+  })
+}
+
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+setupInput();
+handleUserInput()
+
+module.exports= {setupInput}
--- w2/d3/play.js
+++ w2/d3/play.js
@@ -1,8 +1,8 @@
 const net = require('net');
 const stdin = process.stdin;
 
-stdin.setRawMode(true);
-stdin.setEncoding('utf8');
+
+const { setupInput } = require('./input');
 
 
 /**
@@ -10,26 +10,7 @@ stdin.setEncoding('utf8');
  */
 const { connect } = require('./client');
 
-const handleUserInput = function(){
-  stdin.on('data',(key)=>{
-if(key === '\u0003'){
-  process.exit();
-}
-  })
-}
-
-
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-
-setupInput()
-handleUserInput()
-
 console.log('Connecting ...');
 connect();
+
+setupInput();
